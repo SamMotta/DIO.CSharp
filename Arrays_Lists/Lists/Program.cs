f100e1dfd666e7bbda897670d419926367ac671f
SamMotta <samuelsamuca953@gmail.com>
2022-11-11 16:37:48 -0300

    Aprendendo Listas genéricas

---
+++ Arrays_Lists/Lists/Program.cs
@@ -0,0 +1,13 @@
+﻿
+List<string> nomes = new List<string>(); 
+
+nomes.Add("Samuel");
+nomes.Add("André");
+nomes.Add("Ryan");
+nomes.Add("Chico");
+
+nomes.Remove("Samuel");
+foreach(string i in nomes) 
+{
+    Console.WriteLine(i);
+}
