df10a7ae1686d9577fcebc5925160f6fdae29169
SamMotta <samuelsamuca953@gmail.com>
2022-11-11 16:05:36 -0300

    Estruturas de repetição

---
+++ Repetition_Structures/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int numero = 5;
+int i = 0;
+
+// for (int i = 0; i <= 10; i++)
+// {
+//     Console.WriteLine($"{numero + " x " + i} = {numero * i}");
+// }
+
+while (i <= 10)
+{
+    if (i == 0) { i++; continue; }
+    Console.WriteLine($"{numero + " x " + i} = {numero * i}");
+    i++;
+
+    if (i == 6) { break; }
+}
