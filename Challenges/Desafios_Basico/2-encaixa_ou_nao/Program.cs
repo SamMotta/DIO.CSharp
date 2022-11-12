464add3858dde54bde6cc8128d9c39f464063976
SamMotta <samuelsamuca953@gmail.com>
2022-11-11 21:50:24 -0300

    Desafio de Código Básico 2 de 3

---
+++ Challenges/Desafios_Basico/2-encaixa_ou_nao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int qt = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < qt; i++)
+        {
+            string[] arrStr = Console.ReadLine().Split(' ');
+            string a = arrStr[0];
+            string b = arrStr[1];
+
+            if (a.Length < b.Length)
+                Console.WriteLine("nao encaixa");
+            else if (a.EndsWith(b))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+        }
+    }
+}
