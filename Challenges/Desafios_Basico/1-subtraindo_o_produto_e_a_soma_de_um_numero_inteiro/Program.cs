00ac3f1ad819c529685dfea9f63628c12213dc49
SamMotta <samuelsamuca953@gmail.com>
2022-11-11 21:50:12 -0300

    Desafio de Código Básico 1 de 3

---
+++ Challenges/Desafios_Basico/1-subtraindo_o_produto_e_a_soma_de_um_numero_inteiro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DIO
+{
+    static void Main(string[] args)
+    {
+        string numero = Console.ReadLine();
+        int produto = 1, soma = 0;
+
+        for(int i = 0; i < numero.Length; i++)
+        {
+            int digito = Convert.ToInt32(numero[i].ToString());
+            produto *= digito;
+            soma += digito;
+        }
+
+        Console.WriteLine(produto - soma);
+    }
+}
